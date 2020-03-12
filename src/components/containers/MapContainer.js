79fd55b65a3711300a087cf5a74b7683c0992e50
github username <github email address>
2020-03-12 14:31:43 -0400

    set map tiles

--- src/components/containers/MapContainer.js
+++ src/components/containers/MapContainer.js
@@ -6,7 +6,7 @@ const MapContainer = () => {
 
     const [ lat, setLat ] = useState(37.318562)
     const [ long, setLong ] = useState(-121.951071)
-    const [ zoom, setZoom ] = useState(15)
+    const [ zoom, setZoom ] = useState(18)
     const position = [lat, long]
 
 
@@ -14,10 +14,13 @@ const MapContainer = () => {
     return(
         <Map id="map-id" center={position} zoom={zoom}>
             <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                maxZoom={18}
                 edgeBufferTiles={2}
             />
+           
+           
             <Marker position={position}>
             <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
